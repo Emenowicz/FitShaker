804cec5704bf7648cb2028ce0ed6ec3791d123a5
Emenowicz <dmichalowicz96@gmail.com>
2017-06-01 11:20:40 +0200

    Add start/stop accelerometer on button click

--- app/src/main/java/com/example/dawidmichalowicz/fitshaker/MainActivity.java
+++ app/src/main/java/com/example/dawidmichalowicz/fitshaker/MainActivity.java
@@ -5,14 +5,17 @@ import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 
 public class MainActivity extends AppCompatActivity implements SensorEventListener {
@@ -25,9 +28,11 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
     private long lastUpdate = 0;
     private float last_x, last_y, last_z;
     private static final int SHAKE_THRESHOLD = 600;
-    private static final int SHAKE_SLOP_TIME_MS = 500;
+    private static final int SHAKE_SLOP_TIME_MS = 300;
+    private static final int SHAKE_COUNT_RESET_TIME_MS = 3000;
     long shakeTimeStamp;
-    int shakesCounter;
+    int shakesCounter = 0;
+
 
 
     @Override
@@ -35,24 +40,20 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
-        shakesCounter = 0;
 
         sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
         accelerometer = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
-        sensorManager.registerListener(this, accelerometer, SensorManager.SENSOR_DELAY_NORMAL);
-
     }
 
     @Override
     protected void onPause() {
-        super.onPause();
         sensorManager.unregisterListener(this);
+        super.onPause();
     }
 
     @Override
     protected void onResume() {
         super.onResume();
-        sensorManager.registerListener(this, accelerometer, SensorManager.SENSOR_DELAY_NORMAL);
     }
 
 
@@ -78,13 +79,21 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
                     return;
                 }
                 Log.d("Shakestamp>now", "false");
+
+                if(shakeTimeStamp + SHAKE_COUNT_RESET_TIME_MS < now){
+                    shakesCounter = 0;
+                    Log.d("shakesCounter", "set 0");
+                    calCounter.setText(String.valueOf(shakesCounter));
+                }
+
                 if (speed > SHAKE_THRESHOLD) {
                     onShake();
+                    last_x = x;
+                    last_y = y;
+                    last_z = z;
+                    shakeTimeStamp = now;
                 }
-                last_x = x;
-                last_y = y;
-                last_z = z;
-                shakeTimeStamp = now;
+
             }
         }
     }
@@ -94,10 +103,21 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
 
     }
 
+    @OnClick(R.id.start_button)
+    public void startCounting(View view){
+        sensorManager.registerListener(this, accelerometer, SensorManager.SENSOR_DELAY_NORMAL);
+
+
+    }
+
+    @OnClick(R.id.stop_button)
+    public void stopCounting(View view){
+        sensorManager.unregisterListener(this);
+    }
+
     private void onShake() {
         shakesCounter++;
         Log.d("ShakesCounter", "Incremented");
-        System.out.println(shakesCounter);
         calCounter.setText(String.valueOf(shakesCounter));
 
     }
